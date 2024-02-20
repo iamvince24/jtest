7b9eafe9fbf5c2a6495cc32ec72ecb3189a97247
iamvince24 <yohongguo24@gmail.com>
2024-02-20 10:48:34 +0800

    Add SCSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React Now test2
         </a>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </header>
     </div>
   );
